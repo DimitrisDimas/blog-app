80c17b6bc049bd2b6b058e08095d9fdc6d226e64
Dimitris Dimas <didimasmail@gmail.com>
2025-10-27 21:16:56 +0200

    Add find by name method

--- src/main/java/com/springboot/blog/blog_rest_api/repository/RoleRepository.java
+++ src/main/java/com/springboot/blog/blog_rest_api/repository/RoleRepository.java
@@ -3,5 +3,10 @@ package com.springboot.blog.blog_rest_api.repository;
 import com.springboot.blog.blog_rest_api.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role,Long> {
+
+
+    Optional<Role> findByName(String roleUser);
 }
