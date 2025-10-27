9c60b454e58fac0d85257de41f8db076b9c2a68a
Dimitris Dimas <didimasmail@gmail.com>
2025-10-27 20:37:51 +0200

    Add User entity

---
+++ src/main/java/com/springboot/blog/blog_rest_api/entity/User.java
@@ -0,0 +1,37 @@
+package com.springboot.blog.blog_rest_api.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="name",nullable = false)
+    private String name;
+
+    @Column(name="email",nullable = false)
+    private String email;
+
+    @Column(name="password",nullable = false)
+    private String password;
+
+    @Column(name="username",nullable = false)
+    private String username;
+
+}
