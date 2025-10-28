b13892063e4d83b5773711479b20ceccf8da195a
Dimitris Dimas <didimasmail@gmail.com>
2025-10-28 16:54:39 +0200

    Add model mapper bean

--- src/main/java/com/springboot/blog/blog_rest_api/BlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/blog_rest_api/BlogRestApiApplication.java
@@ -2,13 +2,19 @@ package com.springboot.blog.blog_rest_api;
 
 import com.springboot.blog.blog_rest_api.entity.Role;
 import com.springboot.blog.blog_rest_api.repository.RoleRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogRestApiApplication implements CommandLineRunner {
+
+    @Bean
+    public ModelMapper modelMapper(){return new ModelMapper();}
+
 
     public static void main(String[] args) {
         SpringApplication.run(BlogRestApiApplication.class, args);
