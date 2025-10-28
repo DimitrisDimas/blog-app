6496581d07a06243b32c4e588968ccbd6aef282f
Dimitris Dimas <didimasmail@gmail.com>
2025-10-28 02:18:53 +0200

    Added GlobalExceptionHandler with @ControllerAdvice to handle application-wide exceptions

---
+++ src/main/java/com/springboot/blog/blog_rest_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.springboot.blog.blog_rest_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+import java.util.Date;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    //Everytime is called the ResourceNotFoundException the program will show only the necessary information
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+}
