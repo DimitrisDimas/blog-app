7c0b5c6e61cd94f574c89d927ade9c8c176b3b4d
Dimitris Dimas <didimasmail@gmail.com>
2025-10-28 17:43:18 +0200

    Create functionality for delete a post by id(API)

--- src/main/java/com/springboot/blog/blog_rest_api/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/blog_rest_api/service/impl/PostServiceImpl.java
@@ -62,6 +62,14 @@ public class PostServiceImpl implements PostService {
         return mapToDTO(updatedPost);
     }
 
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.delete(post);
+    }
+
+
 
     // --- Mapping helpers ---
     private PostDto mapToDTO(Post post) {
