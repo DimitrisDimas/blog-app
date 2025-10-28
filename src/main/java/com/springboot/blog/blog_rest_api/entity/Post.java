d79a50ba8e90eb65e0fb285ba8da76cb2057a785
Dimitris Dimas <didimasmail@gmail.com>
2025-10-28 16:47:36 +0200

    Create post entitty

---
+++ src/main/java/com/springboot/blog/blog_rest_api/entity/Post.java
@@ -0,0 +1,33 @@
+package com.springboot.blog.blog_rest_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(
+        name ="posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
