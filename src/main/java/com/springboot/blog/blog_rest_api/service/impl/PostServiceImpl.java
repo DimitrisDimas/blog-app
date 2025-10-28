6152811bcb5cc35d91b37f74fa0c9436c94d2671
Dimitris Dimas <didimasmail@gmail.com>
2025-10-28 16:55:16 +0200

    Create mapping helpers methods for the post

--- src/main/java/com/springboot/blog/blog_rest_api/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/blog_rest_api/service/impl/PostServiceImpl.java
@@ -1,15 +1,35 @@
 package com.springboot.blog.blog_rest_api.service.impl;
 
+import com.springboot.blog.blog_rest_api.dto.PostDto;
+import com.springboot.blog.blog_rest_api.entity.Post;
 import com.springboot.blog.blog_rest_api.repository.PostRepository;
 import com.springboot.blog.blog_rest_api.service.PostService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 @Service
 public class PostServiceImpl implements PostService {
-
+    private ModelMapper mapper;
     private PostRepository postRepository;
 
-    public PostServiceImpl(PostRepository postRepository) {
+    public PostServiceImpl(ModelMapper mapper, PostRepository postRepository) {
+        this.mapper = mapper;
         this.postRepository = postRepository;
     }
+
+    
+    // --- Mapping helpers ---
+    private PostDto mapToDTO(Post post) {
+
+        PostDto postDto = mapper.map(post, PostDto.class);
+        return postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto) {
+        Post post = mapper.map(postDto, Post.class);
+        return post;
+    }
+
+
 }
