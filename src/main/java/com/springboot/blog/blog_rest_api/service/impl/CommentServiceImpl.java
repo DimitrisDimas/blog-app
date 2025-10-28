dd9d80078089d9147a8c021e3c3289bab5670b0b
Dimitris Dimas <didimasmail@gmail.com>
2025-10-29 01:55:46 +0200

    Add mapper methods to comment

--- src/main/java/com/springboot/blog/blog_rest_api/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blog/blog_rest_api/service/impl/CommentServiceImpl.java
@@ -1,6 +1,36 @@
 package com.springboot.blog.blog_rest_api.service.impl;
 
+import com.springboot.blog.blog_rest_api.dto.CommentDto;
+import com.springboot.blog.blog_rest_api.entity.Comment;
+import com.springboot.blog.blog_rest_api.repository.CommentRepository;
 import com.springboot.blog.blog_rest_api.service.CommentService;
+import org.modelmapper.ModelMapper;
 
 public class CommentServiceImpl implements CommentService {
+
+    private CommentRepository commentRepository;
+    private ModelMapper mapper;
+
+    public CommentServiceImpl(CommentRepository commentRepository, ModelMapper mapper) {
+        this.commentRepository = commentRepository;
+        this.mapper = mapper;
+    }
+
+
+
+    //Mapper Methods
+    private CommentDto mapToDto(Comment comment){
+        CommentDto commentDto = mapper.map(comment,CommentDto.class);
+        return commentDto;
+    }
+
+    private Comment mapToEntity(CommentDto commentDto){
+        Comment comment = mapper.map(commentDto,Comment.class);
+        return comment;
+    }
+
+
+
+
+
 }
