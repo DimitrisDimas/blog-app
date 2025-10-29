c60bfa03e14de2f67c38ad900393a4701c28e858
Dimitris Dimas <didimasmail@gmail.com>
2025-10-29 03:30:00 +0200

    Updating post and postDto to adjust category in create and update API's

--- src/main/java/com/springboot/blog/blog_rest_api/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/blog_rest_api/service/impl/PostServiceImpl.java
@@ -2,8 +2,10 @@ package com.springboot.blog.blog_rest_api.service.impl;
 
 import com.springboot.blog.blog_rest_api.dto.PostDto;
 import com.springboot.blog.blog_rest_api.dto.PostResponse;
+import com.springboot.blog.blog_rest_api.entity.Category;
 import com.springboot.blog.blog_rest_api.entity.Post;
 import com.springboot.blog.blog_rest_api.exception.ResourceNotFoundException;
+import com.springboot.blog.blog_rest_api.repository.CategoryRepository;
 import com.springboot.blog.blog_rest_api.repository.PostRepository;
 import com.springboot.blog.blog_rest_api.service.PostService;
 import org.modelmapper.ModelMapper;
@@ -18,14 +20,19 @@ import java.util.stream.Collectors;
 public class PostServiceImpl implements PostService {
     private ModelMapper mapper;
     private PostRepository postRepository;
+    private CategoryRepository categoryRepository;
 
-    public PostServiceImpl(ModelMapper mapper, PostRepository postRepository) {
+    public PostServiceImpl(ModelMapper mapper, PostRepository postRepository, CategoryRepository categoryRepository) {
         this.mapper = mapper;
         this.postRepository = postRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
     public PostDto createPost(PostDto postDto) {
+
+        Category category = categoryRepository.findById(postDto.getCategoryId())
+                .orElseThrow(()-> new ResourceNotFoundException("Category","id",postDto.getCategoryId()));
 
         Post post = mapToEntity(postDto);
         Post newPost = postRepository.save(post);
@@ -53,6 +60,10 @@ public class PostServiceImpl implements PostService {
     public PostDto updatePost(PostDto postDto, long id) {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+
+        Category category = categoryRepository.findById(postDto.getCategoryId())
+                .orElseThrow(()-> new ResourceNotFoundException("Category","id",postDto.getCategoryId()));
+
 
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
