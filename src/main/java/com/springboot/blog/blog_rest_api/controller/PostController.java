282e5b1015e6814ab895b5d95dcbfe3ce23d33b6
Dimitris Dimas <didimasmail@gmail.com>
2025-10-29 19:11:42 +0200

    Add input validation for Post entity fields

--- src/main/java/com/springboot/blog/blog_rest_api/controller/PostController.java
+++ src/main/java/com/springboot/blog/blog_rest_api/controller/PostController.java
@@ -3,6 +3,7 @@ package com.springboot.blog.blog_rest_api.controller;
 import com.springboot.blog.blog_rest_api.dto.PostDto;
 import com.springboot.blog.blog_rest_api.dto.PostResponse;
 import com.springboot.blog.blog_rest_api.service.PostService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class PostController {
     }
     // create blog post REST API
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
@@ -38,7 +39,7 @@ public class PostController {
 
     // update post by id
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto,@PathVariable(name="id") long id){
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto,@PathVariable(name="id") long id){
 
         PostDto postResponse = postService.updatePost(postDto,id);
         return new ResponseEntity<>(postResponse,HttpStatus.OK);
