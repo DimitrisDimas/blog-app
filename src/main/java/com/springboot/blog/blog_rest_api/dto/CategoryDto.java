4b48572a10d01acab2ec729d50a76c324f6e65cd
Dimitris Dimas <didimasmail@gmail.com>
2025-10-29 02:57:35 +0200

    Create CategoryDto

--- src/main/java/com/springboot/blog/blog_rest_api/dto/CategoryDto.java
+++ src/main/java/com/springboot/blog/blog_rest_api/dto/CategoryDto.java
@@ -1,4 +1,18 @@
 package com.springboot.blog.blog_rest_api.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDto {
+
+
+    private long id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String description;
 }
