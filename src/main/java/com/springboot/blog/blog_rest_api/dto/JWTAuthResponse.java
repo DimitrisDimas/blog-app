856d024996a59d41b2d5f1972de08ea4aaa71e90
Dimitris Dimas <didimasmail@gmail.com>
2025-11-03 15:20:33 +0200

    create jwt auth response (DTO) - configuring jwt in spring security configuration

---
+++ src/main/java/com/springboot/blog/blog_rest_api/dto/JWTAuthResponse.java
@@ -0,0 +1,17 @@
+package com.springboot.blog.blog_rest_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JWTAuthResponse {
+
+    private String accessToken;
+    private String tokenType ="Bearer";
+
+}
