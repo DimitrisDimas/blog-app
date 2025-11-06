d7aef7b3aa9b7a3d66227c863c05a118e900c35a
Dimitris Dimas <didimasmail@gmail.com>
2025-11-06 18:46:37 +0200

    Add documentation for categoryDto

--- src/main/java/com/springboot/blog/blog_rest_api/dto/CategoryDto.java
+++ src/main/java/com/springboot/blog/blog_rest_api/dto/CategoryDto.java
@@ -1,4 +1,5 @@
 package com.springboot.blog.blog_rest_api.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -11,9 +12,13 @@ public class CategoryDto {
 
 
     private long id;
+
     @NotEmpty(message = "Name should not be null or empty")
+    @Schema(description = "Blog Category Name")
     private String name;
+
     @NotEmpty
     @Size(min = 10, message = "Category description should have at least 10 characters")
+    @Schema(description = "Blog Category Description")
     private String description;
 }
