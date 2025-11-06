6dac49d0b710276486b391f117b7e6e274f0ce64
Dimitris Dimas <didimasmail@gmail.com>
2025-11-06 17:18:30 +0200

    Add documentation for all Categories Api

--- src/main/java/com/springboot/blog/blog_rest_api/controller/CategoryController.java
+++ src/main/java/com/springboot/blog/blog_rest_api/controller/CategoryController.java
@@ -4,6 +4,10 @@ import com.springboot.blog.blog_rest_api.dto.CategoryDto;
 import com.springboot.blog.blog_rest_api.dto.CategoryResponse;
 import com.springboot.blog.blog_rest_api.service.CategoryService;
 import com.springboot.blog.blog_rest_api.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@Tag( name = "REST APIs for Category Resource" )
 public class CategoryController {
 
     CategoryService categoryService;
@@ -21,15 +26,16 @@ public class CategoryController {
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
-
-    //Build add Category Rest API
+    @Operation(summary = "Create Category REST API", description = "Create Category REST API is used to save category into database")
+    @ApiResponse(responseCode = "201", description = "Http Status 201 CREATED")
+    @SecurityRequirement( name = "Bear Authentication" )
     @PostMapping
     public ResponseEntity<CategoryDto> addCategory(@Valid @RequestBody CategoryDto categoryDto){
         CategoryDto savedCategory = categoryService.addCategory(categoryDto);
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
     }
-
-    //Build Get Category REST API
+    @Operation(summary = "Get Category By Id REST API", description = "Get Category By Id REST API is used to get single category from the database")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
     @GetMapping("/{category_id}")
     public ResponseEntity<CategoryDto> getCategory(@PathVariable("category_id") long category_id){
 
@@ -37,7 +43,8 @@ public class CategoryController {
         return ResponseEntity.ok(categoryDto);
     }
 
-    //Build get all Categories Rest Api
+    @Operation(summary = "Get All Categories REST API", description = "Get All Categories REST API is used to fetch all the categories from the database")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
     @GetMapping
     public ResponseEntity<CategoryResponse> getCategories(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
@@ -48,7 +55,9 @@ public class CategoryController {
         return ResponseEntity.ok(categoryService.getAllCategories(pageNo, pageSize, sortBy, sortDir));
     }
 
-    //Update category by id
+    @Operation(summary = "update Category REST API", description = "Update Category REST API is used to update a particular category in the database")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
+    @SecurityRequirement(name = "Bear Authentication")
     @PutMapping("/{category_id}")
     public ResponseEntity<CategoryDto> updateCategory(
             @PathVariable("category_id") long category_id,
@@ -58,7 +67,9 @@ public class CategoryController {
         return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
     }
 
-    //Delete Category by id
+    @Operation(summary = "Delete Category REST API", description = "Delete Category REST API is used to delete a particular category from the database")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
+    @SecurityRequirement(name = "Bear Authentication")
     @DeleteMapping("/{category_id}")
     public ResponseEntity<String> deleteCategory(@PathVariable("category_id") long category_id){
         categoryService.deleteCategory(category_id);
