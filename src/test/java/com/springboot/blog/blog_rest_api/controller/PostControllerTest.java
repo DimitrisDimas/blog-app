d98d65f3c6d1997be61526e07ad618ad4f261175
Dimitris Dimas <didimasmail@gmail.com>
2025-11-12 02:51:11 +0200

    Testing get api for getting all the posts

--- src/test/java/com/springboot/blog/blog_rest_api/controller/PostControllerTest.java
+++ src/test/java/com/springboot/blog/blog_rest_api/controller/PostControllerTest.java
@@ -3,6 +3,7 @@ package com.springboot.blog.blog_rest_api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.blog.blog_rest_api.dto.PostDto;
+import com.springboot.blog.blog_rest_api.dto.PostResponse;
 import com.springboot.blog.blog_rest_api.security.JwtTokenProvider;
 import com.springboot.blog.blog_rest_api.service.PostService;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -68,6 +70,64 @@ public class PostControllerTest {
                 .andExpect(jsonPath("$.content").value("This is the content of my first post. It has enough length."))
                 .andExpect(jsonPath("$.categoryId").value(1));
     }
+
+    @Test
+    public void testGetAllPosts_ReturnsPaginatedPosts() throws Exception {
+        // Arrange
+        int pageNo = 0;
+        int pageSize = 2;
+        String sortBy = "title";
+        String sortDir = "asc";
+
+        // Mock post data
+        PostDto post1 = new PostDto();
+        post1.setId(1L);
+        post1.setTitle("First Post");
+        post1.setDescription("Description of first post");
+        post1.setContent("Content of first post");
+        post1.setCategoryId(1L);
+
+        PostDto post2 = new PostDto();
+        post2.setId(2L);
+        post2.setTitle("Second Post");
+        post2.setDescription("Description of second post");
+        post2.setContent("Content of second post");
+        post2.setCategoryId(2L);
+
+        List<PostDto> posts = Arrays.asList(post1, post2);
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(posts);
+        postResponse.setPageNo(pageNo);
+        postResponse.setPageSize(pageSize);
+        postResponse.setTotalElements(2L);
+        postResponse.setTotalPages(1);
+        postResponse.setLast(true);
+
+        // Mock service
+        Mockito.when(postService.getAllPosts(pageNo, pageSize, sortBy, sortDir)).thenReturn(postResponse);
+
+        // Act & Assert
+        mockMvc.perform(get("/api/posts")
+                        .param("pageNo", String.valueOf(pageNo))
+                        .param("pageSize", String.valueOf(pageSize))
+                        .param("sortBy", sortBy)
+                        .param("sortDir", sortDir)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content.length()").value(2))
+                .andExpect(jsonPath("$.content[0].id").value(1))
+                .andExpect(jsonPath("$.content[0].title").value("First Post"))
+                .andExpect(jsonPath("$.content[1].id").value(2))
+                .andExpect(jsonPath("$.content[1].title").value("Second Post"))
+                .andExpect(jsonPath("$.pageNo").value(pageNo))
+                .andExpect(jsonPath("$.pageSize").value(pageSize))
+                .andExpect(jsonPath("$.totalElements").value(2))
+                .andExpect(jsonPath("$.totalPages").value(1))
+                .andExpect(jsonPath("$.last").value(true));
+    }
+
 
     // ----------- Invalid Test -----------
     @Test
